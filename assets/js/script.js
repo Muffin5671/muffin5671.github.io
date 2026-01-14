ea20f7f789f957208959c9d7bf0a1a26252d981d
Muffin5671 <geometrydashmeltrightdown@gmail.com>
2026-01-14 11:54:49 -0500

    Update script.js

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,3 +1,4 @@
 console.log("%cWelcome to Muffin's Tools",'font-family: Roboto; font-size: 25px; text-align: center; background-color: #7aa0ff; color: #ffffff; padding: 32px 48px;');
 console.log('%cnote: i really only added this because i love the console styling feature','font-family: Roboto; font-size: 16px; text-align: center; background-color: #eeeeee; color: #363636; padding: 8px 32px;');
-console.log('%cyou may or may not have seen pixi js do it okay bye','font-family: Roboto; font-size: 16px; text-align: center; background-color: #eeeeee; color: #363636; padding: 8px 32px;');
+console.log("%cyou may or may not have seen pixi js do it uhhhhhhh here's my yt okay bye",'font-family: Roboto; font-size: 16px; text-align: center; background-color: #eeeeee; color: #363636; padding: 8px 32px;');
+console.log('%chttps://www.youtube.com/@MuffinGDYT','font-family: Roboto; font-size: 16px; font-weight: 500; text-align: center; background-color: #e62c2c; color: #ffffff; padding: 12px 48px; border-radius: 4px; cursor: pointer;');
