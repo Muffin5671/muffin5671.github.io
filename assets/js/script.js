05276c54eb969408d152e4c4f73b590373de5290
Muffin5671 <geometrydashmeltrightdown@gmail.com>
2026-01-14 11:46:21 -0500

    Create script.js

---
+++ assets/js/script.js
@@ -0,0 +1,3 @@
+console.log("%cWelcome to Muffin's Tools",'font-family: Roboto; font-size: 25px; text-align: center; background-color: #7aa0ff; color: #ffffff; padding: 32px 48px;');
+console.log('%cnote: i really only added this because i love the console styling feature','font-family: Roboto; font-size: 16px; text-align: center; background-color: #eeeeee; color: #363636; padding: 8px 32px;');
+console.log('%cyou may or may not have seen pixi js do it okay bye','font-family: Roboto; font-size: 16px; text-align: center; background-color: #eeeeee; color: #363636; padding: 8px 32px;');
