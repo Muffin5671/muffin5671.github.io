0b587e9b17c91465a7d648c9ae247db8d684609f
Muffin5671 <geometrydashmeltrightdown@gmail.com>
2026-01-17 17:13:47 -0500

    Update script.js

--- assets/js/script.js
+++ assets/js/script.js
@@ -3,7 +3,7 @@ console.log('%cnote: i really only added this because i love the console styling
 console.log("%cyou may or may not have seen pixi js do it uhhhhhhh here's my yt okay bye",'font-family: Roboto; font-size: 16px; text-align: center; background-color: #eeeeee; color: #363636; padding: 8px 32px;');
 console.log('%chttps://www.youtube.com/@MuffinGDYT','font-family: Roboto; font-size: 16px; font-weight: 500; text-align: center; background-color: #e62c2c; color: #ffffff; padding: 12px 48px; border-radius: 4px; cursor: pointer;');
 
-function editErrorText() {
+window.onload = function editErrorText() {
   document.getElementById('errortext').innerHTML = "The requested file " 
   + document.location.pathname + 
   " was not hosted on the server. (404)"
