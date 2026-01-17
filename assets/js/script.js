d3c6e06fa5e6f1e86f611610ba5e6a0d05460819
Muffin5671 <geometrydashmeltrightdown@gmail.com>
2026-01-17 16:34:41 -0500

    Update script.js

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,3 +2,9 @@ console.log("%cWelcome to Muffin's Tools",'font-family: Roboto; font-size: 25px;
 console.log('%cnote: i really only added this because i love the console styling feature','font-family: Roboto; font-size: 16px; text-align: center; background-color: #eeeeee; color: #363636; padding: 8px 32px;');
 console.log("%cyou may or may not have seen pixi js do it uhhhhhhh here's my yt okay bye",'font-family: Roboto; font-size: 16px; text-align: center; background-color: #eeeeee; color: #363636; padding: 8px 32px;');
 console.log('%chttps://www.youtube.com/@MuffinGDYT','font-family: Roboto; font-size: 16px; font-weight: 500; text-align: center; background-color: #e62c2c; color: #ffffff; padding: 12px 48px; border-radius: 4px; cursor: pointer;');
+
+function editErrorText() {
+  document.getElementById('errortext').innerHTML = "The requested file " 
+  + document.location.pathname + 
+  " was not hosted on the server. (404)"
+}
