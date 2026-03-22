1ad9757d8c9b7a75d066904b292b365b65c43491
Muffin5671 <213143890+Muffin5671@users.noreply.github.com>
2026-03-22 16:38:29 -0400

    update

--- assets/js/script.js
+++ assets/js/script.js
@@ -3,11 +3,11 @@ console.log('%cnote: i really only added this because i love the console styling
 console.log("%cyou may or may not have seen pixi js do it uhhhhhhh here's my yt okay bye",'font-family: Roboto; font-size: 16px; text-align: center; background-color: #eeeeee; color: #363636; padding: 8px 32px;');
 console.log('%chttps://www.youtube.com/@MuffinGDYT','font-family: Roboto; font-size: 16px; font-weight: 500; text-align: center; background-color: #e62c2c; color: #ffffff; padding: 12px 48px; border-radius: 4px; cursor: pointer;');
 
-window.onload = function editErrorText() {
-  document.getElementById('errortext').innerHTML = "The requested file " 
-  + document.location.pathname + 
-  " was not hosted on the server. (404)"
+function editErrorText() {
+  document.getElementById('errortext').innerHTML = `The requested file ${document.location.pathname} was not hosted on the server. (404)`;
 }
+
+window.onload = editErrorText;
 
 function darkMode() {
   document.getElementsByTagName('html')[0].style.backgroundColor = '#222222';
